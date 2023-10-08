73afd1aede108167a7d0bfc83eb8247c05817dcd
Edwinkaniamuthan <147285185+Edwinkaniamuthan@users.noreply.github.com>
2023-10-08 05:59:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function checkPattern() {
+  var userInput = document.getElementById("password").value; 
+  var pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/; 
+
+  if (pattern.test(userInput)) {
+      alert("login successful");
+  } else {
+      alert("login failed"); 
+      return false; 
+  }
+}
